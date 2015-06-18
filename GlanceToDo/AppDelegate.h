4b563aa4d198ec9c5d84573f3ce861b6d36cc298
leokwanbt14 <leokwanbt14@gmail.com>
2015-06-18 13:39:56 -0400

    added interface

---
+++ GlanceToDo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GlanceToDo
+//
+//  Created by Leo Kwan on 6/18/15.
+//  Copyright (c) 2015 Leo Kwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
