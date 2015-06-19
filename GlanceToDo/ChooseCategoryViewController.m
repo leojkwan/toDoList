6a495cef4313ed88f580365bf70ab147aa668994
leokwanbt14 <leokwanbt14@gmail.com>
2015-06-19 18:13:04 -0400

    modified font and logo

--- GlanceToDo/ChooseCategoryViewController.m
+++ GlanceToDo/ChooseCategoryViewController.m
@@ -29,7 +29,7 @@
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    - m
+    
 
 
 }
--- GlanceToDo/HomeTableViewController.m
+++ GlanceToDo/HomeTableViewController.m
@@ -137,6 +137,12 @@
 }
 
 
+
+//-(BOOL)prefersStatusBarHidden{
+//    return YES;
+//}
+
+
 /*
 // Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
